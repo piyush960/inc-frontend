f13bca2774ab624b3190d39b96a10a6af4978002
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2022-12-22 14:00:51 +0530

    Adjusted exports in index.js files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,6 @@
-import Homepage from './homepage.jsx';
-import EventDetails from './eventDetails.jsx';
-import Auth from './auth.jsx';
-import Forms from './forms.jsx';
-import Dashboard from './dashboard.jsx';
-import InCTeams from './incTeams.jsx';
-
-const Pages = {
-    Homepage,
-    EventDetails,
-    Auth,
-    Forms,
-    Dashboard,
-    InCTeams,
-}
-
-export default Pages;
+export { default as Homepage } from './homepage.jsx';
+export { default as EventDetails } from './eventDetails.jsx';
+export { default as Auth } from './auth.jsx';
+export { default as Forms } from './forms.jsx';
+export { default as Dashboard } from './dashboard.jsx';
+export { default as InCTeams } from './incTeams.jsx';
