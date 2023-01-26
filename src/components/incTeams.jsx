4c66b58d9c226b5e8eceb51c83301e6d073973cd
Suyash Udchan <suyash.udchan@gmail.com>
2023-01-27 01:52:05 +0530

    OurTeam

--- src/components/incTeams.jsx
+++ src/components/incTeams.jsx
@@ -1,9 +1,13 @@
 import './styles/incTeams.css';
+import { Link } from "react-router-dom";
 
 function InCTeamsSection() {
     return (
-        <div className='inc-teams-component'>
-        </div>
+        <><h2 className="title">MEET OUR TEAM</h2><div className="container">
+           
+            <div className="btn btn-border-4"><Link to="./pages/inc-teams">OUR TEAM</Link></div>
+            <a className="btn btn-border-5" href="">I'm a button</a>
+        </div></>
     );
 }
 
