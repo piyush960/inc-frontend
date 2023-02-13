46a41e74626d530240c6378bb68e4dddcdf1da86
Advait Naik <naikadvait1610@gmail.com>
2023-02-13 23:47:35 +0530

    small changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -27,7 +27,7 @@ function Footer() {
     </div>
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
     <div class="sm:flex sm:items-center sm:justify-between lg:px-52">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 INC™, All Rights Reserved.
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">©PICT InC 2023 All Rights Reserved | Developed by WEB TEAM of InC
         </span>
         <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
             <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
