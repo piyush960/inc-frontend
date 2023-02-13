995d1fab197ad95681457ed5ac04f1a9bb65f243
hpkoolkarni <hpkoolkarni11.in@gmail.com>
2023-02-13 14:40:42 +0530

    judges form added

--- src/components/forms/registrations/team_concepts.jsx
+++ src/components/forms/registrations/team_concepts.jsx
@@ -1,9 +1,11 @@
 import '../styles/event_registrations.css';
+import InputBox from '../../inputBox';
 
 function TeamConcepts({ team, groupDetails }) {
     return (
         <div>
             
+           
         </div>
     );
 }
--- src/pages/conceptsForms.jsx
+++ src/pages/conceptsForms.jsx
@@ -5,12 +5,12 @@ const conlogo = require("./concepts23_logo_bw.png");
 function ConceptsForm() {
     const MAX_STEPS = 2;
     const [activeStep, setActiveStep] = useState(0)
-    const [modalState, setModalState] = useState(false)
+    const [showModal, setShowModal] = useState(false)
     const [team, setTeam] = useState([])
     const [groupDetails, setGroupDetails] = useState({})
 
     function toggleInstructionsModal() {
-        setModalState(prevState => !prevState)
+        setShowModal(prevState => !prevState)
     }
 
     function nextStep() {
@@ -18,19 +18,57 @@ function ConceptsForm() {
     }
 
     function prevStep() {
-        if (activeStep > 0)  setActiveStep(activeStep - 1)
+        if (activeStep > 0) setActiveStep(activeStep - 1)
     }
     //project_details {}
 
     return (
         <div className='forms'>
-            <FormsBanner logo={conlogo} eventName="CONCEPTS" eventDescription="24th and 25th March"/>
+            <FormsBanner logo={conlogo} eventName="CONCEPTS" eventDescription="24th and 25th March" />
+            <div className=''>
+                <Buttons value="i" onClick={toggleInstructionsModal} classNames="rounded-full ml-44" />
+            </div>
+
             <ProgressBar />
-            <Modals modalState={modalState} toggleModal={toggleInstructionsModal} >
+            <Modals modalState={showModal} toggleModal={toggleInstructionsModal} >
                 <>
+                <div className="flex justify-center items-center overflow-x-hidden text-black overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                  <h3 className="text-3xl font=semibold">Instructions</h3>
+                  <button
+                    className="bg-transparent border-0 text-black float-right"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
+                      x
+                    </span>
+                  </button>
+                </div>
+                <div className="relative p-6 flex-auto">
+                 <ul>
+                    <li>Instruction No 1</li>
+                    <li>Instruction No 2</li>
+                    <li>Instruction No 3</li>
+                    <li>Instruction No 4</li>
+                 </ul>
+                </div>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
                 </>
-            </Modals>
-            <Buttons onClick={toggleInstructionsModal} />
+        </Modals>
+
             <TeamConcepts team={team} groupDetails={groupDetails} />
             {/*ProjectDetails props=setProjectDetails*/}
             {/*Next Step btns*/}
