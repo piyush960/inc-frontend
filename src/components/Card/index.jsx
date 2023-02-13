04d1d7499e6901b849371e96a9a12dd82778050e
Manas Kshatriya <manaskshatriya01@gmail.com>
2023-02-13 19:20:31 +0530

    home page changes

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,9 @@
 function Card(props){
     return (
-        <div className="card">       
+        <div className="card   ">       
             {/* my card*/}
-                <div className="py-10">
-                    <div className="rounded overflow-hidden shadow-lg max-w-sm bg-cyan-800 hover:scale-105 cursor-pointer">
+                <div className="my-10">
+                    <div className="  rounded overflow-hidden shadow-lg max-w-sm bg-cyan-800 hover:scale-105 hover:shadow-xl hover:shadow-light_blue cursor-pointer">
                         <img src ="https://reactjs.org/logo-og.png" 
                         alt="" 
                         className="w-full"/>
--- src/components/eventCards.jsx
+++ src/components/eventCards.jsx
@@ -4,9 +4,9 @@ import eventData from './card/eventData';
 
 function EventCards() {
     return (
-        <div className='eventCards'>
+        <div className='eventCards my-10 '>
           <h1 className="mt-10 mb-2 text-center capitalize text-4xl font-bold text-white">Events</h1>
-          <hr className="w-1/5 mx-auto"/>
+          <hr className="w-1/5 mx-auto mb-5 "/>
           <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 lg:gap-3 justify-items-center'>
             {
               eventData.map( (eva) =>{
