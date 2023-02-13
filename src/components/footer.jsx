f14dbd7b1015454b9bbf6caa65e0a54f266e45ab
Advait Naik <naikadvait1610@gmail.com>
2023-02-13 23:38:59 +0530

    small changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -18,43 +18,7 @@ function Footer() {
                 <img src={logo} className="h-20 w-20 mr-3" alt="INC logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">INC <span className="self-center text-xs font-semibold whitespace-nowrap dark:text-white">Impetus And Concepts</span></span>
             </a>
-        </div>
-        {/* <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-                <ul class="text-gray-600 dark:text-gray-400">
-                    <li class="mb-4">
-                        <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
-                    </li>
-                    <li>
-                        <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                <ul class="text-gray-600 dark:text-gray-400">
-                    <li class="mb-4">
-                        <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
-                    </li>
-                    <li>
-                        <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                <ul class="text-gray-600 dark:text-gray-400">
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                    </li>
-                </ul>
-            </div>
-        </div> */}
-                    
+        </div>      
 
                     <div className="Email lg:mt-8">
                         <h1 className="mb-2 text-2xl text-gray-900 uppercase dark:text-white">EMAIL</h1>
