0cb906087016f7b564e814673bf20a7e66a11a3a
Maithili <maithileetawde@gmail.com>
2023-03-13 00:49:44 +0530

    schedule style update

--- src/components/eventCards.jsx
+++ src/components/eventCards.jsx
@@ -74,7 +74,7 @@ function Card(props) {
 function EventCards() {
   return (
     <div className='eventCards my-10 '>
-      <h1 className="mt-10 mb-2 text-center capitalize text-4xl font-bold text-white">Events</h1>
+      <h1 className="mt-10 mb-2 text-center capitalize text-5xl font-bold text-white">Events</h1>
       <hr className="w-1/5 mx-auto mb-5 " />
       <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 lg:gap-3 justify-items-center'>
         {
