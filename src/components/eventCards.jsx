69f07cfad2cb4a87422e826c7c538340fd10267a
hpkoolkarni <hpkoolkarni11.in@gmail.com>
2023-04-06 20:56:28 +0530

    eventdetails and card changes

--- src/components/eventCards.jsx
+++ src/components/eventCards.jsx
@@ -11,25 +11,26 @@ const eventData = [
     fees: 200,
     members: "2-5 members",
     prize: 1000,
+    ct: "First year,Second year and third year students",
     logo: impetus_logo,
     nt: "₹ 100/- For National Entries",
     it: "Free for International Entries",
-    contact : [{
-      name : "Apoorvaraj",
-      phone : "8530191073",
+    contact: [{
+      name: "Apoorvaraj",
+      phone: "8530191073",
     },
     {
       name: "Mrugank",
-      phone : "7083823772"
+      phone: "7083823772"
 
     },
     {
       name: "Vrushali",
-      phone : "9766176681"
+      phone: "9766176681"
     },
     {
       name: "Aarti",
-      phone :  "9405119460",
+      phone: "9405119460",
     }
     ]
   },
@@ -40,25 +41,27 @@ const eventData = [
     fees: 300,
     members: "2-5 members",
     prize: 1000,
+    ct: "Final year students only    ",
     logo: concepts_logo,
+
     nt: "₹ 300/- For National Entries",
     it: "Free for International Entries",
-    contact : [{
-      name : "Siddharth",
-      phone : "8237892072",
+    contact: [{
+      name: "Siddharth",
+      phone: "8237892072",
     },
     {
       name: "Kalpesh",
-      phone : "7769945077"
+      phone: "7769945077"
 
     },
     {
       name: "Yamini",
-      phone : "7385190784"
+      phone: "7385190784"
     },
     {
       name: "Vishakha",
-      phone :  "9011628404",
+      phone: "9011628404",
     },
     ]
   },
@@ -69,26 +72,19 @@ const eventData = [
     fees: 100,
     members: "Max 2 Members",
     prize: 1000,
+    ct: "First, second, third and forth year students",
     logo: pradnya_logo,
     nt: "₹ 100/- For National Entries",
     it: "Free for International Entries",
-    contact : [{
-      name : "Pratik",
-      phone : "9145439727",
+    contact: [{
+      name: "Pratik",
+      phone: "9145439727",
     },
     {
       name: "Neha",
-      phone : "9579678142"
+      phone: "9579678142"
 
     },
-    // {
-    //   name: "Vrushali",
-    //   phone : "9766176681"
-    // },
-    // {
-    //   name: "Aarti",
-    //   phone :  "9405119460",
-    // }
     ]
   },
 ];
@@ -97,16 +93,16 @@ function Card(props) {
   const redirectEvents = useNavigate()
 
   return (
-    <div className="card hover:cursor-pointer" onClick={() => window.open('/event-details/' + props.name.toLowerCase(),'_blank')}>
+    <div className="card hover:cursor-pointer" onClick={() => window.open('/event-details/' + props.name.toLowerCase(), '_blank')}>
       {/* my card*/}
       <div className="py-8 mx-5 md:mx-0">
-        <div className="md:h-[670px] shadow-md shadow-light_blue/20 hover:bg-light_blue hover:scale-105 transition ease-in-out  bg-light_blue/30 rounded-xl  border-light_blue items-center p-4 md:p-8 border md:mx-5  mt-10">
-        <div className='flex'>
+        <div className="md:h-[750px] shadow-md shadow-light_blue/20 hover:bg-light_blue hover:scale-105 transition ease-in-out  bg-light_blue/30 rounded-xl  border-light_blue items-center p-4 md:p-8 border md:mx-5  mt-10">
+          <div className='flex'>
             <div className=''>
               <img src={props.logo}
-              alt=""
-              className="w-full" />
-              </div>
+                alt=""
+                className="w-full" />
+            </div>
             <div className="my-auto text-xl mr-10 md:text-3xl font-poppins group text-gold font-bold tracking-wider decoration-1 decoration-light_blue uppercase">{props.name}</div>
 
           </div>
@@ -115,12 +111,32 @@ function Card(props) {
             <hr className="mt-2 text-light_blue" />
 
             {/* <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio magni dolorem libero pariatur nisi fugit voluptate perferendis, harum sapiente sit eius excepturi, similique repellendus explicabo dicta a ipsa iure temporibus dolorum, accusantium consequatur sequi possimus! Quas non veniam aliquid et?</p> */}
+            <p className="my-4  text-lg md:text-xl border-l-2 border-gold md:font-light md:leading-6  pl-2">
+              {" "}
+              <div className="flex"><span className="font-bold "> TEAM</span> 
+              <div className="border-dotted border-l-2 border-light_blue/20 ml-9 pl-2">{props.team}</div> </div>
+              
+            </p>
             <p className="my-4 text-lg md:text-xl border-l-2 border-gold md:font-light md:leading-6  pl-2">
               {" "}
-              <span className="font-bold "> TEAM</span> : {props.team}
+              <div className="flex"><span className="font-bold "> CRITERIA</span> 
+              <div className="border-dotted border-l-2 border-light_blue/20 ml-1 pl-2">{props.ct}</div> </div>
+              
             </p>
-
-            <div className="flex gap-3 my-10 ">
+          
+            
+            {/* <div className="flex gap-3 my-10 ">
+              <p className="my-4 text-lg md:text-xl flex items-baseline  border-l-2 border-gold md:font-light md:-leading-6   pl-2">
+                {" "}
+                <span className="font-bold -rotate-90  w-20   "> CRITERIA  </span>
+              </p>
+              <div className="border-dotted border-l-2 border-light_blue/20 ">
+                <p className="my-4 text-lg md:text-xl   md:font-light md:leading-6  pl-2">
+                  {props.ct}
+                </p>
+              </div>
+            </div> */}
+            <div className="flex gap-3 my-5 ">
               <p className="my-4 text-lg md:text-xl flex items-baseline  border-l-2 border-gold md:font-light md:-leading-6   pl-2">
                 {" "}
                 <span className="font-bold -rotate-90  w-20   "> FEES  </span>
@@ -138,7 +154,7 @@ function Card(props) {
             <div className=" flex gap-3  ">
               <p className=" my-1 text-lg md:text-xl flex place-items-center   border-l-2 border-gold md:font-light md:-leading-6   pl-2">
                 {" "}
-                <span className="font-bold -rotate-90 w-20 "> Contacts </span>
+                <span className="font-bold -rotate-90 w-20 "> CONTACT </span>
               </p>
               <div className="border-dotted border-l-2  border-light_blue/20 ">
                 <ul>
@@ -146,7 +162,7 @@ function Card(props) {
                     props.contact.map((item) => {
                       return (
                         <li className="my-1 px-4" >
-                          <strong>{item.name} : </strong>{item.phone}
+                          <p className="text-lg md:text-xl font-medium">{item.name} : </p >{item.phone}
                         </li>
                       )
                     })
@@ -193,6 +209,7 @@ function EventCards() {
               ne={eva.nt}
               ie={eva.it}
               contact={eva.contact}
+              ct={eva.ct}
             />
           );
         })}
