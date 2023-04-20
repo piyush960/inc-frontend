8257d74dd935ef978c42b062acf7bad6a6493461
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2023-04-21 03:16:48 +0530

    Judges Done

--- src/pages/judge/judgeAllocation.jsx
+++ src/pages/judge/judgeAllocation.jsx
@@ -39,7 +39,7 @@ function JudgeAllocation() {
               data?.data["concepts"]?.map((project) => (
                 <div
                   key={project.pid}
-                  className="rounded-lg outline-dashed outline-2 outline-offset-[3px] my-2 outline-light_blue px-4 py-2 bg-faint_blue/10 mb-3 w-[100%] md:w-[100%]"
+                  className="rounded-lg outline-dashed outline-2 outline-offset-[3px] my-2 outline-light_blue px-4 py-2 bg-faint_blue/10 mb-3 w-[100%] md:w-[90%]"
                 >
                   <div className="flex ">
                     <h1 className="text-lg md:text-xl font-bold text-gold">
@@ -96,7 +96,7 @@ function JudgeAllocation() {
               data?.data["impetus"].map((project) => (
                 <div
                   key={project.pid}
-                  className="rounded-lg outline-dashed outline-2 outline-offset-[3px] my-2 outline-light_blue px-4 py-2 bg-faint_blue/10 mb-3 w-[90%] md:w-[90%]"
+                  className="rounded-lg outline-dashed outline-2 outline-offset-[3px] my-2 outline-light_blue px-4 py-2 bg-faint_blue/10 mb-3 w-[100%] md:w-[90%]"
                 >
                   <div className="flex ">
                     <h1 className="md:text-xl text-lg font-bold text-gold">
