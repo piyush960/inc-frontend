f34aaee1d7958f34335fb32a1b05dd8444c409fc
hpkoolkarni <hpkoolkarni11.in@gmail.com>
2023-04-20 17:22:10 +0530

    schedule and result changes

--- src/components/schedule.jsx
+++ src/components/schedule.jsx
@@ -3,7 +3,7 @@ import { Fade } from 'react-awesome-reveal';
 
 function Schedule() {
   return (
-    <div id='schedule' className='md:m-20 p-2 flex flex-col justify-evenly items-stretch'>
+    <div id='schedule' className='md:my-20 ml-20 p-2 flex flex-col justify-evenly items-stretch'>
       <span className='w-full text-center text-4xl md:text-5xl text-white my-10 drop-shadow-xl font-poppins '>
         <Fade delay={100} duration={1000}>
           Schedule
@@ -24,9 +24,11 @@ function Schedule() {
               <div class='mt-3  sm:pr-8'>
                 <h3 class='text-3xl font-semibold text-gold dark:text-gold md:text-justify text-center'>DAY 1</h3>
                 <time class='block mb-1 text-xl font-normal leading-none text-gold dark:text-gold'>21st April'23</time>
-                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷9:30 AM – Reporting to Shamiyana</p>
-                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷10:00 AM to 11:30 AM-Inauguration</p>
-                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷11:30 AM to 6:00 PM – Judging & Commencement of Exhibition.</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷9:30 AM – Registration and Reporting to Shamiyana</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷10:00 AM to 11:00 AM-Inauguration</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷11:30 AM to 7:00 PM –Concepts : Judging & Exhibition.</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷11:30 AM to 7:00 PM –Impetus : Judging.</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷11:30 AM to 4:30 PM –Pradnya : Round 1.</p>
                 {/* <p class='text-base font-normal text-white dark:text-white'>Get started with dozens of web components and interactive elements.</p> */}
               </div>
             {/* </li>
@@ -42,9 +44,12 @@ function Schedule() {
                 <h3 class='text-3xl font-semibold text-gold dark:text-gold md:text-justify text-center'>DAY 2</h3>
                 <time class='block mb-1 text-xl font-normal leading-none text-gold dark:text-gold'>22nd April'23</time>
                 {/* <p class='text-base font-normal text-white dark:text-white'>Get started with dozens of web components and interactive elements.</p> */}
-                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷9:00 AM to 6:00 PM - Judging/Exhibition</p>
-              </div>
-            {/* </li>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷9:00 AM to 7:00 PM - Judging/Exhibition</p> 
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷11:30 AM to 7:00 PM –Concepts : Judging & Exhibition.</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'> 🔷11:30 AM to 7:00 PM –Impetus : Judging.</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷9:00 AM to 3 PM -Pradnya : Round 2</p>
+              <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷4 PM -Pradnya : Judging Round</p>
+              </div> {/* </li>
             <li class='relative mb-6 sm:mb-0 '> */}
               {/* <div class='flex items-center'>
                 <div class='z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-light_blue/40 dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 hover:border-faint_blue hover:bottom-2 group shadow-lg max-w-sm rounded-3xl hover:bg-transparent hover:text-gold   hover:scale-105 cursor-pointer  hover:shadow-light_blue hover:shadow-xl '>
@@ -55,8 +60,8 @@ function Schedule() {
               <div class='mt-3 sm:pr-8 '>
                 <h3 class='text-3xl font-semibold text-gold dark:text-gold md:text-justify text-center'>DAY3</h3>
                 <time class='block mb-1 text-xl font-normal leading-none text-gold dark:text-gold text-justify'>23rdApril'23</time>
-                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷9:00 AM to 11:30 AM – Judging (if any projects left for judging)</p>
-                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷11:30 AM to 4:30 PM – Evaluation</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷9:00 AM to 11:30 AM – Judging</p>
+                <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷11:30 AM to 4:30 PM – Consolidation and preparation of winner's list</p>
                 <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷4:30 PM – Reporting to Shamiyana</p>
                 <p class='block text-xl font-normal leading-none text-gold dark:text-gold py-1'>🔷5:00 PM to 7:20 PM – Closing Ceremony</p>
                 {/* <p class='text-base font-normal text-white dark:text-white'>Get started with dozens of web components and interactive elements.</p> */}
