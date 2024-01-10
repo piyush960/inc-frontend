1dbca45e44bdd4cddf6270e4647a343e0203d2b5
Viraj <viraj.sonawane22@pccoepune.org>
2024-01-10 13:07:14 +0530

    INC_2024

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -61,45 +61,32 @@ const AboutUs = () => (
         <h1 className="text-center">About Us</h1>
       </header>
       <div className="about-body relative overflow-hidden py-12">
-        <div className="about-content text-left w-[90%] md:w-8/12 z-10 p-8 md:px-12 text-lg shadow-2xl shadow-light_blue/20 bg-[#000]/80 rounded-xl border border-light_blue/30">
-          Impetus and Concepts (InC) is a flagship technical event of SCTR's
-          Pune Institute of Computer Technology (PICT) Pune, which will be held
-          in during 3rd week of March 2024. InC is an intercollegiate
-          International level competition which has been catching the attention
-          of corporate giants for Quality of projects and an opportunity to
-          recruit/mentor young talented budding entrepreneurs. Every year InC
-          sets a New Benchmark and provides an opportunity to students to
-          realize their ideas into effective product. Over the years, it has
-          become most popular & awaited event with continuous improvement in
-          Footfall, Number and Quality of projects/papers, etc. This event also
-          sets a platform for students to design, exhibit and watch their ideas
-          come true. This technical fest has Inventive events namely - Impetus,
-          Concepts, Pradnya.
-          <br />
-          <br />
-          Accordingly,Impetus Project Competition FE to TE students, All
-          Engineering Branches confined to specific Domains,Concepts Projects
-          CompetitionFinal Year Students,All Engineering Branches confined
-          Specific Domains & Pradnya - International Coding Competition.
-          <br />
-          <br />
-          <span className="">
-            Events are invited with the Project addressing the societal needs
-            like Health, Agriculture, Kids/Women Safety, Education etc , and the
-            best project judged by the juries will be awarded with a cash prize
-            of{" "}
-            <span className="font-extrabold underline italic text-white">
-              Rs. 1 Lac Cash Prize
-            </span>{" "}
-            from PICT
-            <br />
-            🔹Judge's decision will be final.
-            <br />
-            🔹Project status must be in "Ready to Use"
-            <br />
-            🔹Already registered candidatess need not register again.
-          </span>
-        </div>
+      <div className="about-content text-justify w-[90%] md:w-8/12 z-10 p-8 md:px-12 text-lg shadow-2xl shadow-light_blue/20 bg-[#000]/80 rounded-xl border border-light_blue/30">
+  <p>
+    Impetus and Concepts (InC) is a flagship technical event of SCTR's Pune Institute of Computer Technology (PICT) Pune, which will be held during the 1st week of April 2024. InC is an intercollegiate international level competition that has been catching the attention of corporate giants for the quality of projects and an opportunity to recruit/mentor young talented budding entrepreneurs. Every year InC sets a new benchmark and provides an opportunity for students to realize their ideas into effective products. Over the years, it has become the most popular and awaited event with continuous improvement in footfall, the number and quality of projects/papers, etc. This event also sets a platform for students to design, exhibit, and watch their ideas come true. This technical fest has inventive events namely - Impetus, Concepts, Pradnya.
+  </p>
+  <br />
+  <br />
+  <p>
+    Accordingly, Impetus Project Competition for FE to TE students, all engineering branches confined to specific domains, Concepts Projects Competition for Final Year Students, all engineering branches confined to specific domains, and Pradnya - International Coding Competition.
+  </p>
+  <br />
+  <br />
+  <span className="">
+    Events are invited with the project addressing the societal needs like Health, Agriculture, Kids/Women Safety, Education, etc., and the best project judged by the juries will be awarded with a cash prize of{" "}
+    <span className="font-extrabold underline italic text-white">
+      Rs. 1 Lac Cash Prize
+    </span>{" "}
+    from PICT
+    <br />
+    🔹Judge's decision will be final.
+    <br />
+    🔹Project status must be in "Ready to Use"
+    <br />
+    🔹Already registered candidates need not register again.
+  </span>
+</div>
+
 
         <div className="tag-list w-full blur-sm hover:blur-none transition duration-300">
           {[...new Array(ROWS)].map((_, i) => (
