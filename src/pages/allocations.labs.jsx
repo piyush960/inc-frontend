80817006ca48159b1a4d58e12c5a399bc171969c
ompanchwate2003 <ompanchwate2003@gmail.com>
2024-04-05 22:48:11 +0530

    lab allocation page updated

--- src/pages/allocations.labs.jsx
+++ src/pages/allocations.labs.jsx
@@ -6,7 +6,6 @@ const Table = lazy(() => import('../components/table.jsx'))
 
 function ProjectsLabAllocation() {
     const { data: conceptsData, isLoading: isConceptsLoading } = useGetLabAllocations('concepts')
-    // const { data: impetusData, isLoading: isImpetusLoading } = useGetLabAllocations('impetus')
 
     const columns = [
         {
@@ -30,8 +29,8 @@ function ProjectsLabAllocation() {
             sortable: true
         },
         {
-            name: 'JUDGES ASSIGNED FOR FIRST TWO DAYS',
-            cell: row => row['jids'].map(jid => <div key={jid}>{jid} , &nbsp; </div>),
+            name: 'JUDGES ASSIGNED FOR DAY TWO',
+            cell: row => row['jids']?.join(', '),
             ignoreRowClick: true,
             grow: 2,
             width: '1000px',
@@ -40,8 +39,7 @@ function ProjectsLabAllocation() {
 
     return (
         <>
-            <FormsBanner eventName='Project-To-Lab Allocations' eventDescription='Please find below the list of project id and allocated judge id for first two days.
-' />
+            <FormsBanner eventName='Project-To-Lab Allocations' eventDescription='Please find below the list of project id and allocated judge id for day two ( 6th April 2024 ).' />
             <h1 className='text-4xl text-center mt-6 text-gold font-bold'>Concepts</h1>
             {!isConceptsLoading ?
                 <Suspense fallback={<div>Loading Allocations...</div>}>
@@ -57,13 +55,15 @@ function ProjectsLabAllocation() {
                     />
                 </Suspense> :
                 <div className='text-center text-2xl'>Loading Allocations...</div>
+
+
             }
-            <div className='mx-5 lg:mx-20 mt-5'>
-                <div className='w-full rounded-lg outline-dashed outline-2 outline-offset-[3px] outline-light_blue px-4 py-2 bg-faint_blue/10 mb-3 text-lg'>
+
+            <div className='mx-5 md:mx-20 lg:mx-40 mt-5'>
+                <div className='w-full rounded-lg outline-dashed outline-2 outline-offset-[3px] outline-light_blue px-4 py-2 bg-faint_blue/10 mb-3 text-lg text-justify'>
                     <span className='font-bold text-faint_blue/80 italic pr-2 text-justify'>NOTE : </span>
-                    You are requested to visit the schedule along with lab allocation regularly on the website in case of any deviation. The schedule is also emailed to the team leader. Apart from the above allocation, there may be many visitors, judges, company delegates who will access your project for various awards, internships, pre-placement offers/placement, etc. 
-                    &nbsp; &nbsp; In case of any queries please contact Sarthak at : 
-                    <span className='text-[#5a5abd]'>  &nbsp; 
+                    You are requested to visit the schedule along with lab allocation regularly on the website in case of any deviation. The schedule is also emailed to the team leader. Apart from the above allocation, there may be many visitors, judges, company delegates who will access your project for various awards, internships, pre-placement offers/placement, etc. In case of any queries please contact Sarthak at :
+                    <span className='text-[#5a5abd]'>  &nbsp;
                         <a href='tel:+918180909092' className='text-blue-500 font-bold'>+91 8180909092</a>
                     </span>
                 </div>
@@ -74,14 +74,8 @@ In case of any queries please contact Sarthak at +91 8180909092" /> */}
             </div>
 
 
-            {/* <hr className='border border-gold md:w-[70%] w-full m-auto' /> */}
-            {/* <h1 className='text-4xl text-center mt-6 text-gold font-bold'>Impetus</h1>
-            {!isImpetusLoading ?
-                <Suspense fallback={<div>Loading Allocations...</div>}>
-                    <Table title={`Impetus Lab Allocations ${new Date().toISOString().split('T')[0]}`} columns={columns} loading={isImpetusLoading} data={impetusData?.data} outerClassName='md:mx-auto mb-10 md:w-[50%] w-full' />
-                </Suspense> :
-                <div className='text-center text-2xl'>Loading Allocations...</div>
-            } */}
+
+
         </>
     )
 }
