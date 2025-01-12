c45e32d0e809d7b11dc500e140f07223efd63094
piyush960 <piyushdahake096@gmail.com>
2025-01-12 23:35:11 +0530

    ui bubble pos fix

--- src/components/HeroParallax.jsx
+++ src/components/HeroParallax.jsx
@@ -41,22 +41,22 @@ const Hero = ({ lightOn }) => {
 
       <p className='absolute sm:left-[50%] top-[10.5%] sm:top-[12%] sm:translate-x-[-50%] uppercase text-slate-400 font-light text-center text-md sm:text-lg'><span className=''>SCTR&apos;s Pune Institute of Computer Technology</span><span className='sm:block text-center'>&nbsp;Presents</span></p>
 
-      <div className='relative flex flex-col items-center sm:h-full justify-center sm:w-[600px]'>
+      <div className='relative flex flex-col items-center justify-center sm:w-[600px]'>
 
-        <img src={impetus_b} alt="impetus_logo" className='sm:w-24 sm:h-24 w-11 h-11 absolute bottom-[11.8%] left-[1.2%] sm:bottom-[21%] sm:left-[-4%] z-10 cursor-pointer hover:scale-105 duration-300 opacity-70 hover:opacity-100' onClick={() => navigate('/events/impetus')}/>
+        <img src={impetus_b} alt="impetus_logo" className='sm:w-24 sm:h-24 w-11 h-11 absolute bottom-[11.8%] left-[1.2%] sm:bottom-[25.4%] sm:left-[-4%] z-10 cursor-pointer hover:scale-105 duration-300 opacity-70 hover:opacity-100' onClick={() => navigate('/events/impetus')}/>
 
-        <img src={pradnya_b} alt="pradnya_logo" className='sm:w-24 sm:h-24 w-11 h-11 absolute left-[45%] top-[9.8%] sm:top-[15%] sm:left-[42%] z-10 cursor-pointer hover:scale-105 duration-300 opacity-70 hover:opacity-100' onClick={() => navigate('/events/pradnya')}/>
+        <img src={pradnya_b} alt="pradnya_logo" className='sm:w-24 sm:h-24 w-11 h-11 absolute left-[45%] top-[9.8%] sm:top-[20.3%] sm:left-[42%] z-10 cursor-pointer hover:scale-105 duration-300 opacity-70 hover:opacity-100' onClick={() => navigate('/events/pradnya')}/>
 
-        <img src={concepts_b} alt="concepts_logo" className='sm:w-24 sm:h-24 w-9 h-9 absolute top-[44.5%] right-[-2.5%] sm:top-[41.4%] sm:right-[-8%] rotate-[18deg] z-10 cursor-pointer opacity-70 hover:opacity-100 hover:scale-105 duration-300' onClick={() => navigate('/events/concepts')}/>
+        <img src={concepts_b} alt="concepts_logo" className='sm:w-24 sm:h-24 w-9 h-9 absolute top-[44.5%] right-[-2.5%] sm:top-[42.6%] sm:right-[-8%] rotate-[18deg] z-10 cursor-pointer opacity-70 hover:opacity-100 hover:scale-105 duration-300' onClick={() => navigate('/events/concepts')}/>
         
         <h1 className='relative sm:mt-8'>
           <span className='text-white font-bold text-lg sm:text-5xl absolute top-[14%] left-[3.8%] sm:top-[20.6%] sm:left-[11.4%] pointer-events-none'>PICT</span>
           <TextHoverEffect text={'INC'} />
-          <span className='text-[170px] sm:hidden text-white-100/55   font-[Helvetica] font-bold tracking-tight'>INC</span>
+          <span className='text-[170px] sm:hidden text-white-100/55 font-[Helvetica] font-bold tracking-tight'>INC</span>
           <span className='text-white font-bold text-lg sm:text-5xl absolute bottom-[14%] right-[4%] sm:bottom-[29%] sm:right-[12%] pointer-events-none'>2K25</span>
         </h1>
 
-        <div className='absolute bottom-[-10%] sm:bottom-[8.3%] w-[93%] sm:w-[100%] z-10 hover:scale-[1.03] active:scale-95 duration-300'>
+        <div className='absolute bottom-[-10%] sm:bottom-[14.8%] w-[93%] sm:w-[100%] z-10 hover:scale-[1.03] active:scale-95 duration-300'>
           <motion.button
             style={{
               border,
@@ -70,8 +70,8 @@ const Hero = ({ lightOn }) => {
         </div>
 
       </div>
-      <div className='flex sm:flex-[0.65] flex-col-reverse max-sm:h-1/2 sm:flex-col sm:justify-center items-center max-sm:-mb-8 mt-10 xl:-mr-9 relative sm:h-full'>
-        {lightOn && <img src={light} alt="light" className='absolute object-cover pointer-events-none top-[-10%] translate-x-[50%] right-[50%] z-10 opacity-35 max-sm:hidden'/>}
+      <div className='flex sm:flex-[0.65] flex-col-reverse max-sm:h-1/2 sm:flex-col sm:justify-center items-center max-sm:-mb-8 mt-10 xl:-mr-9 relative'>
+        {lightOn && <img src={light} alt="light" className='absolute object-cover pointer-events-none top-[-28%] translate-x-[50%] right-[50%] z-10 opacity-35 max-sm:hidden'/>}
         <div className='w-[240px] h-[240px] sm:w-[400px] sm:h-[400px] relative'>
           <IncCanvas />
         </div>
